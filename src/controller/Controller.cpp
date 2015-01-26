fe864212e207819a4a2fc6750c791acc18adea37
Maciej Korpalski <korpalski.maciej@gmail.com>
2015-01-26 08:32:12 +0000

    Mapped event and strategies

--- src/controller/Controller.cpp
+++ src/controller/Controller.cpp
@@ -8,10 +8,36 @@
 
 Controller::Controller()
 {
-/*    this->strategyMap.insert(std::make_pair<std::type_index, StringStrategy*>
-            (std::type_index(typeid(StringEvent)), new StringStrategy()));
-    this->strategyMap.insert(std::make_pair<std::type_index, PartStrategy*>
-            (std::type_index(typeid(PartEvent)), new PartStrategy()));*/
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingAllResourcesStrategy*>
+            (std::type_index(typeid(OutgoingAllResourcesRequestEvent)), new OutgoingAllResourcesStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingAllResourcesStrategy*>
+            (std::type_index(typeid(IncomingAllResourcesRequestEvent)), new IncomingAllResourcesStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingResourceRequestStrategy*>
+            (std::type_index(typeid(OutgoingResourceRequestEvent)), new OutgoingResourceRequestStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingResourceRequestStrategy*>
+            (std::type_index(typeid(IncomingResourceRequestEvent)), new IncomingResourceRequestStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingRevokeStrategy*>
+            (std::type_index(typeid(IncomingRevokeRequestEvent)), new IncomingRevokeStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingRevokeStrategy*>
+            (std::type_index(typeid(OutgoingRevokeRequestEvent)), new OutgoingRevokeStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingRevertStrategy*>
+            (std::type_index(typeid(IncomingRevertRequestEvent)), new IncomingRevertStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingRevertStrategy*>
+            (std::type_index(typeid(OutgoingRevertRequestEvent)), new OutgoingRevertStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingPartStrategy*>
+            (std::type_index(typeid(IncomingPartEvent)), new IncomingPartStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingPartStrategy*>
+            (std::type_index(typeid(OutgoingPartEvent)), new OutgoingPartStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingPartRequestStrategy*>
+            (std::type_index(typeid(IncomingPartRequestEvent)), new IncomingPartRequestStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingPartRequestStrategy*>
+            (std::type_index(typeid(OutgoingPartRequestEvent)), new OutgoingPartRequestStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, IncomingResourceInformationStrategy*>
+            (std::type_index(typeid(IncomingResourceInformationEvent)), new IncomingResourceInformationStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, OutgoingResourceInformationStrategy*>
+            (std::type_index(typeid(OutgoingResourceInformationEvent)), new OutgoingResourceInformationStrategy()));
+    this->strategyMap.insert(std::make_pair<std::type_index, TransformDownloadedResourceStrategy*>
+            (std::type_index(typeid(TransformEvent)), new TransformDownloadedResourceStrategy()));
 }
 
 Controller::~Controller() {}
